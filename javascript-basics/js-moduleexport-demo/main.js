5ffebc60a55062d94d4050469fc1ba97136c2c42
miura0405 <keisuke.miura@prum.jp>
2025-07-10 23:37:05 +0900

    Create12 js-module-export directory with HTML and JavaScript for module-export demonstrationfeat: エクスポートデモと README の説明を追加

---
+++ javascript-basics/js-moduleexport-demo/main.js
@@ -0,0 +1,6 @@
+import { greet, farewell } from './module1.js'; //関数名
+import add from './module2.js'; 
+
+console.log(greet("太郎")); 
+console.log(farewell("太郎"));
+console.log(add(3, 5));
---
+++ javascript-basics/js-moduleexport-demo/module1.js
@@ -0,0 +1,8 @@
+// 名前付きexport を使用
+export const greet = (name) => {
+  return `こんにちは、${name}さん`;
+};
+
+export const farewell = (name) => {
+  return `さようなら、${name}さん`;
+};
